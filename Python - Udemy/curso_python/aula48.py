2cfb2a881a3b29e86fa886f0fe4bf4fb883dc524
nando798 <nando798@live.com>
2025-01-06 15:45:45 -0300

    06.01.2025

---
+++ Python - Udemy/curso_python/aula48.py
@@ -0,0 +1,16 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+"""
+
+#            +01234
+#            -54321
+
+string = "ABCDE"
+lista = [123, 456, "fernando", True, []]
+print(lista , type(lista))
+lista[2] = "casa da maria joana"
+print(lista[2], type(lista[2]))
---
+++ Python - Udemy/curso_python/aula49.py
@@ -0,0 +1,20 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis:
+    append, insert, pop, del, clear, extend, +
+Create Read Update   Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
+"""
+
+numeros = [1,2,5678,4]
+del numeros[1]
+numeros [2] = 6753
+numeros.append(345)
+numeros.append(2435)
+numeros.append(4532)
+numeros.append(3452)
+print(numeros)
+pop(numeros[123453, 324231,352,65432,2345,3252])
