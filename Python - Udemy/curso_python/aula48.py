b1686e2d4331407ffe803d51c688ed3b084169eb
nando798 <nando798@live.com>
2025-01-07 13:12:22 -0300

    07.01.2025

--- Python - Udemy/curso_python/aula48.py
+++ Python - Udemy/curso_python/aula48.py
@@ -6,11 +6,67 @@ Conhecimentos reutilizáveis - índices e fatiamento
 Métodos úteis: append, insert, pop, del, clear, extend, +
 """
 
-#            +01234
-#            -54321
+# #            +01234
+# #            -54321
 
-string = "ABCDE"
-lista = [123, 456, "fernando", True, []]
-print(lista , type(lista))
-lista[2] = "casa da maria joana"
-print(lista[2], type(lista[2]))
+# string = "ABCDE"
+# lista = [123, 456, "fernando", True, []]
+# print(lista , type(lista))
+# lista[2] = "casa da maria joana"
+# print(lista[2], type(lista[2]))
+
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis:
+    append, insert, pop, del, clear, extend, +
+Create Read Update   Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
+"""
+
+# numeros = [10,20,30,40,50,60,70]
+# numeros.pop(2)
+
+# numeros.append(80)
+# numeros.append("Fernando")
+# nome = numeros.pop()
+# numeros.insert(4, 5)
+# print(numeros, nome)
+
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis:
+    append - Adiciona um item ao final
+    insert - Adiciona um item no índice escolhido
+    pop - Remove do final ou do índice escolhido
+    del - apaga um índice
+    clear - limpa a lista
+    extend - estende a lista
+    + - concatena listas
+Create Read Update   Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
+"""
+lista_a = [1,2,3]
+lista_b = [4,5,6]
+lista_c = lista_a + lista_b
+lista_a.extend(lista_b)
+print(lista_a)
+
+nome = "fernando"
+outro_nome = nome
+nome = "Joao"
+print(nome)
+print(outro_nome)
+
+lista_a =["Fernando", "Silvina", 1, 2.8]
+lista_b = lista_a.copy()
+
+lista_a[0] = "Dalva e Neide"
+
+print(lista_b)
+print(lista_a)
--- Python - Udemy/curso_python/aula49.py
+++ Python - Udemy/curso_python/aula49.py
@@ -1,20 +1,9 @@
 """
-Listas em Python
-Tipo list - Mutável
-Suporta vários valores de qualquer tipo
-Conhecimentos reutilizáveis - índices e fatiamento
-Métodos úteis:
-    append, insert, pop, del, clear, extend, +
-Create Read Update   Delete
-Criar, ler, alterar, apagar = lista[i] (CRUD)
+for in com listas
 """
+listas = ["Fernando", "Neide", "Dalva"]
+listas_indice = listas
+print(listas_indice)
 
-numeros = [1,2,5678,4]
-del numeros[1]
-numeros [2] = 6753
-numeros.append(345)
-numeros.append(2435)
-numeros.append(4532)
-numeros.append(3452)
-print(numeros)
-pop(numeros[123453, 324231,352,65432,2345,3252])
+
+    
